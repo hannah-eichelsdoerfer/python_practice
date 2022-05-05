6b875c8f26ae12cb4ee8b83c4b861f6384ef7348
Hannah <hannaheichelsdoerfer@gmail.com>
2022-05-05 18:02:10 +0200

    extra exercise allowed_to_vote

---
+++ Exercises/extra.py
@@ -0,0 +1,19 @@
+# Step 1: Create a function called `allowed_to_vote` that takes in a `age` as an argument
+# TODO: return (not print!) a boolean stating whether `age` is old enough to vote (18+)
+# NOTE: Use an if/else statment
+
+
+
+# Step 2: Ask the user for their age
+
+
+
+# Step 3: Call the function with the age they entered
+
+
+
+# Step 4: Depending on the restult, print out whether they are allowed to vote or not
+
+
+
+# To check if it's working, run this file and enter your age and see if you can vote!
