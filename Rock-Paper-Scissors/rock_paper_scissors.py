61068a4e7dac66fc597f1ea14b05b7ad506bfd70
Hannah <hannaheichelsdoerfer@gmail.com>
2022-05-05 14:57:55 +0200

    rock paper scissor instructions

---
+++ Rock-Paper-Scissors/rock_paper_scissors.py
@@ -0,0 +1,17 @@
+# Rock Paper Scissors Game
+
+# Remember the rules:
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
+# If the same, it's a tie
+
+# Write a program so that you can play rock, paper, scissors with the computer.
+# The program should randomly select rock, paper, or scissors for the computer.
+# Get the user to input either rock, paper, or scissors.
+# The program should then compare the user's choice to the computer's choice
+# Let the programm print out the result stating who won.
+
+# Advanced
+# Let the user type in again and again until they type in something to quit
+# Keep track how of many times each player won
